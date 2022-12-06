4f5bf33ae3fa3f0ecbacbf11fcb12be907fe6f00
User <User@User-PC>
2022-12-07 00:03:35 +0900

    AddOperater 수정

--- src/main/java/com/pro/lee/controller/AddOperater.java
+++ src/main/java/com/pro/lee/controller/AddOperater.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AddOperater {
 
-	@RequestMapping("/add/{num1}/{num2}")
-		public int addOperator(@PathVariable int num1, @PathVariable int num2) {
-		return num1 + num2;
+	@RequestMapping("/add/{num1}/{num2}/{num3}")
+		public int addOperator(@PathVariable int num1, @PathVariable int num2, @PathVariable int num3) {
+		return num1 + num2 + num3;
 	}
 }
